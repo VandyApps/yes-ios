4e79bd915613a056e50d9a8568629a0024969d9c
lane <lanekelly16@gmail.com>
2012-10-30 22:40:26 -0500

    refactoring

---
+++ yes-ios/LinkCell.h
@@ -0,0 +1,13 @@
+//
+//  LinkCell.h
+//  yes-ios
+//
+//  Created by Lane on 10/30/12.
+//  Copyright (c) 2012 VandyMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinkCell : UITableViewCell
+
+@end
